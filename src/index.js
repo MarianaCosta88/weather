a87083289f170cfeaa465d0dd057771773489a37
MarianaCosta88 <marianadematoscosta@gmail.com>
2024-10-08 11:10:48 +0100

    inject the forecast HTML from JS

--- src/index.js
+++ src/index.js
@@ -16,6 +16,8 @@ function refreshWeather(response) {
   windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
   temperatureElement.innerHTML = Math.round(temperature);
   iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="weather-app-icon" />`;
+
+    getForecast(response.data.city);
 }
 
 function formatDate(date) {
@@ -52,7 +54,43 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function getForecast(city) {
+  let apiKey= "a2bdd2030t54e74e3o536da4bcfe314e";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
+  axios(apiUrl).then(displayForecast);
+}
+
+function displayForecast(response) {
+  console.log(response.data);
+
+  let days= [`Tue`,`Wed`,`Thu`,`Fri`,`Sat`,`Sun`];
+  let forecastHtml="";
+
+days.forEach(function (day) {
+  forecastHtml= 
+  forecastHtml + 
+  `
+<div class="weather-forecast-day">
+  <div class= "weather-forecast-date">${day}</div>
+  <div class="weather-forecast-icon">☀️</div>
+  <div class="weather-forecast-temperatures">
+  <div class="weather-forecast-temperature"><strong>15º</strong>
+  </div>
+  <div class="weather-forecast-temperature">9º</div>
+  </div>
+  </div>
+  `;
+
+});
+
+let forecastElement =document.querySelector("#forecast");
+forecastElement.innerHTML=forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Porto");
+
+
+
