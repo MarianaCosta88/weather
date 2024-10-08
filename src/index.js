26e01912b2bc3f63e528cac62b7096254a6d3506
MarianaCosta88 <marianadematoscosta@gmail.com>
2024-10-08 12:20:37 +0100

    API Integration

--- src/index.js
+++ src/index.js
@@ -54,6 +54,14 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
+
 function getForecast(city) {
   let apiKey= "a2bdd2030t54e74e3o536da4bcfe314e";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
@@ -61,36 +69,34 @@ function getForecast(city) {
 }
 
 function displayForecast(response) {
-  console.log(response.data);
+  let forecastHtml = "";
 
-  let days= [`Tue`,`Wed`,`Thu`,`Fri`,`Sat`,`Sun`];
-  let forecastHtml="";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
+      <div class="weather-forecast-day">
+        <div class="weather-forecast-date">${formatDay(day.time)}</div>
 
-days.forEach(function (day) {
-  forecastHtml= 
-  forecastHtml + 
-  `
-<div class="weather-forecast-day">
-  <div class= "weather-forecast-date">${day}</div>
-  <div class="weather-forecast-icon">☀️</div>
-  <div class="weather-forecast-temperatures">
-  <div class="weather-forecast-temperature"><strong>15º</strong>
-  </div>
-  <div class="weather-forecast-temperature">9º</div>
-  </div>
-  </div>
-  `;
+        <img src="${day.condition.icon_url}" class="weather-forecast-icon" />
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>${Math.round(day.temperature.maximum)}º</strong>
+          </div>
+          <div class="weather-forecast-temperature">${Math.round(
+            day.temperature.minimum
+          )}º</div>
+        </div>
+      </div>
+    `;
+    }
+  });
 
-});
-
-let forecastElement =document.querySelector("#forecast");
-forecastElement.innerHTML=forecastHtml;
+  document.getElementById("forecast").innerHTML = forecastHtml;
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Porto");
-
-
-
